72c39a8dfa83d33e626de1d3ac6017edcb00ba2b
bryandebourbon <bryandebourbon@gmail.com>
2025-01-01 15:11:26 -0500

    Add/update file Tests/HollyJollyShowTests/DummyTests.swift

---
+++ Tests/HollyJollyShowTests/DummyTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import HollyJollyShow
+
+final class DummyTests: XCTestCase {
+    func testExample() throws {
+        // This is a dummy test so SwiftPM sees a Tests target.
+        XCTAssertTrue(true)
+    }
+}
