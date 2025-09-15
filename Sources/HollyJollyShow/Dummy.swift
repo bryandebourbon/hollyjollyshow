f34646c4d75f93f077121219f3e99f724e59760e
Bryan de Bourbon <bryandebourbon@gmail.com>
2025-09-14 21:13:20 -0400

    removing bloat

--- Sources/HollyJollyShow/Dummy.swift
+++
@@ -1,2 +0,0 @@
-// Dummy.swift
-// This file ensures SwiftPM recognizes 'Sources/HollyJollyShow' as a valid target directory.
--- Tests/HollyJollyShowTests/DummyTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import HollyJollyShow
-
-final class DummyTests: XCTestCase {
-    func testExample() throws {
-        // This is a dummy test so SwiftPM sees a Tests target.
-        XCTAssertTrue(true)
-    }
-}
